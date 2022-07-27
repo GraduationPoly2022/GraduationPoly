e5d524ee924653e7d8e8917e475d2e48bcaa008b
MinhLe <0010minhle@gmail.com>
2022-07-27 15:06:47 +0700

    update bang2

--- src/main/java/com/shop/entity/Category.java
+++ src/main/java/com/shop/entity/Category.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table("categories")
+@Table(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
