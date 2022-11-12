0e58f68cef8c39f47e3f2062c2c3c5bbf962541b
MinhLe <0010minhle@gmail.com>
2022-11-12 22:49:55 +0700

    task 7 update

--- src/main/java/com/shop/controller/StatisticalController.java
+++ src/main/java/com/shop/controller/StatisticalController.java
@@ -22,17 +22,17 @@ public class StatisticalController {
     //Statistics of 10 best-selling products by month and year
     public ResponseEntity<ResponseMessage> thongKe_Top10(@RequestParam("month") Integer month, @RequestParam("year") Integer year) {
         List<Object[]> list = this.iStatisticalService.thongKeTop10(month, year);
-        return getStatisticalRevenue(list);
+        return getStatisticalRevenueTop10(list);
     }
 
     @GetMapping("/top-10-by-year/{year}")
     //Statistics of 10 best-selling products by year
     public ResponseEntity<ResponseMessage> thongKe_Top10ByYear(@PathVariable("year") Integer year) {
         List<Object[]> list = this.iStatisticalService.thongKeTop10ByYear(year);
-        return getStatisticalRevenue(list);
+        return getStatisticalRevenueTop10(list);
     }
 
-    public ResponseEntity<ResponseMessage> getStatisticalRevenue(List<Object[]> list) {
+    public ResponseEntity<ResponseMessage> getStatisticalRevenueTop10(List<Object[]> list) {
         List<Statistical_Top10Dto> list_top10 = new ArrayList<>();
         for (Object[] objects : list) {
             list_top10.add(Convert.objectToClass(objects, Statistical_Top10Dto.class));
